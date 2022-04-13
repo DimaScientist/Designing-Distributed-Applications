1a28e8cebe8189b343e8ffb9d26c249dfe7caf22
Dmitry Bakanov <dima.bakanov.1999@mail.ru>
2022-04-13 08:36:09 +0400

    add base example for lab2

--- Lab2/base_example.py
+++ Lab2/base_example.py
@@ -1,22 +1,19 @@
 from loguru import logger
 
-from config import Config
 from src.chord_node import ChordNode
 from src.utils import stabilisation
 
-
 if __name__ == "__main__":
-    configuration = Config()
-
-    logger.info(f"Создаем узлы в количестве: {configuration.NUMBER_NODE} и количество бит: {configuration.BYTE_LENGTH}")
+    logger.info("Пример из методички:")
+    m = 3
 
     node_list = []
-    head_node = ChordNode(0, configuration.BYTE_LENGTH)
+    head_node = ChordNode(0, m)
     head_node.join(None)
     node_list.append(head_node)
 
-    for n in range(1, configuration.NUMBER_NODE):
-        chord_node = ChordNode(n, configuration.BYTE_LENGTH)
+    for i in [1, 3]:
+        chord_node = ChordNode(i, m)
         chord_node.join_to_node(head_node)
         node_list.append(chord_node)
 
@@ -24,7 +21,7 @@ if __name__ == "__main__":
     stabilisation(node_list)
 
     logger.info("Добавим узел...")
-    new_node = ChordNode(configuration.NUMBER_NODE + 1, configuration.BYTE_LENGTH)
+    new_node = ChordNode(6, m)
     new_node.join_to_node(head_node)
     node_list.append(new_node)
     stabilisation(node_list)
@@ -35,5 +32,3 @@ if __name__ == "__main__":
     new_node.delete()
     stabilisation(node_list)
     logger.info("Узел удален успешно")
-
-
--- Lab2/src/utils.py
+++ Lab2/src/utils.py
@@ -1,3 +1,29 @@
+from loguru import logger
+from tqdm import tqdm
+
+
 def generate_finger_start(n: int, m: int, i: int) -> int:
     """Генерация стартового значения для Finger."""
-    return (n + 2**i) % 2**m
+    return (n + 2 ** i) % 2 ** m
+
+
+def log_nodes(nodes) -> None:
+    """Печать узлов в качестве логов."""
+    logger.debug("Узлы:")
+    for node in nodes:
+        logger.debug(f"{str(node)}")
+
+
+def stabilisation(nodes) -> None:
+    """Стабилизация узлов в списке."""
+    logger.info("Стабилизируем узлы...")
+
+    iterations = len(nodes) * len(nodes) + 1
+    for i in tqdm(range(iterations)):
+        for node in nodes:
+            node.stabilize()
+            node.fix_fingers()
+
+    logger.info("Стабилизация окончена")
+
+    log_nodes(nodes)
